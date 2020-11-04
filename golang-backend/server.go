8933d616d73e2c12fa48dfe2e08ddb7735b4ee69
Rhiskey <first3395@gmail.com>
2020-11-04 14:49:30 +0300

    added vue

--- golang-backend/server.go
+++ golang-backend/server.go
@@ -4,6 +4,8 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"log"
+	"net/http"
 
 	_ "github.com/lib/pq"
 )
@@ -55,8 +57,26 @@ func init() {
 }
 
 func main() {
+	http.HandleFunc("/", homePageHandler)
+
+	fmt.Println("Server listening on port 3000")
+	log.Panic(
+		http.ListenAndServe(":3000", nil),
+	)
+	//TODO: Connect to pg
 	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
 		"password=%s dbname=%s sslmode=disable",
 		host, port, user, password, dbname)
 	fmt.Println(psqlInfo)
+}
+
+func homePageHandler(w http.ResponseWriter, r *http.Request) {
+	_, err := fmt.Fprintf(w, "hello world")
+	checkError(err)
+}
+
+func checkError(err error) {
+	if err != nil {
+		log.Panic(err)
+	}
 }
